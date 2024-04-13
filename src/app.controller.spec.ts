7970798ae72e166f1d774418a2c99362cdc8c146
Andre Oliveira <andre.pbo@gmail.com>
2024-04-13 11:28:03 -0300

    refactor(test): change describe root string

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "Hello, you are in the test environment"', () => {
+      expect(appController.getHello()).toBe('Hello, you are in the test environment');
     });
   });
 });
